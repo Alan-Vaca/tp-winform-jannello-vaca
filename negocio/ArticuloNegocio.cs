57353127c3fa48fbeedbdcf94871c1e0cfa1fb21
Alan-Vaca <avaca@request.com.ar>
2021-09-15 20:02:35 -0300

    creación de librerías de clases y conexión base
    se crean las libs Dominio y Negocio con la funcionalidad base

---
+++ negocio/ArticuloNegocio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    class ArticuloNegocio
+    {
+        AccesoDatos AccesoDatos = new AccesoDatos("(local)\\SQLEXPRESS", "CATALOGO_DB");
+    }
+}
